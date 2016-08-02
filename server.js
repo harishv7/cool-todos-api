567eda7508ad70c7403b205675fc3081f16676bf
Harish V <harishv207@gmail.com>
2016-08-02 20:57:48 +0800

    Add PUT todos route

--- server.js
+++ server.js
@@ -54,6 +54,36 @@ app.delete('/todos/:id', function(req, res) {
 	}
 });
 
+// Update todos
+app.put('/todos/:id', function(req, res) {
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(!matchedTodo) {
+		res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(404).send();
+	}
+
+	// objects in JS are passed by reference, not value. So we don't need to update it again.
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+
+});
+
 // POST request - can take data /todos
 // requires body-parser npm module
 app.post('/todos', function(req, res) {
